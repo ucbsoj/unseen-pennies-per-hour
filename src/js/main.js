2375a5fe15d25d607bc6e63a50e25ba849dfd384
Lucio Villa <lucio310@gmail.com>
2019-04-29 19:50:51 -0700

    updated archival photos and added captions

--- src/js/main.js
+++ src/js/main.js
@@ -16,39 +16,51 @@ $(document).scroll(function() {
   // this code, like the one below
   // switches to the next image and if scrolled up it switches to the previous image
   if (scroll > $('#scroll-img-two').offset().top){
-    $(".img-scroller img").attr("src","./assets/archival/2.jpg")
+    $(".img-scroller img").attr("src","./assets/archival/2.jpg");
+    $('.img-scroller .caption').html('The sewing area at California Institution of Women, 1953. Photo courtesy of CDCR.')
   }else if(scroll < $('#scroll-img-two').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/1.jpg")
+    $('.img-scroller .caption').html('San Quentin dining hall, undated. Photo courtesy of CDCR.')
   }
 
   if (scroll > $('#scroll-img-three').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/3.jpg")
+    $('.img-scroller .caption').html('Kitchen in California Medical Facility, 1981. Photo courtesy of CDCR.')
   }else if(scroll < $('#scroll-img-three').offset().top && scroll >$('#scroll-img-two').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/2.jpg")
+    $('.img-scroller .caption').html('The sewing area at California Institution of Women, 1953. Photo courtesy of CDCR.')
   }
 
   if (scroll > $('#scroll-img-four').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/4.jpg")
+    $('.img-scroller .caption').html('A tobacco factory was moved from San Quentin to California Men’s Colony West in 1958.')
   }else if(scroll < $('#scroll-img-four').offset().top && scroll >$('#scroll-img-three').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/3.jpg")
+    $('.img-scroller .caption').html('Kitchen in California Medical Facility, 1981. Photo courtesy of CDCR.')
   }
 
   if (scroll > $('#scroll-img-five').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/5.jpg")
+    $('.img-scroller .caption').html('Fire camp inmates learn rescuing techniques. Photo estimated to be taken during the 1960s. Photo courtesy of CDCR.')
   }else if(scroll < $('#scroll-img-five').offset().top && scroll >$('#scroll-img-four').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/4.jpg")
+    $('.img-scroller .caption').html('A tobacco factory was moved from San Quentin to California Men’s Colony West in 1958.')
   }
 
   if (scroll > $('#scroll-img-six').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/6.jpg")
+    $('.img-scroller .caption').html('Inmates process tobacco at a factory at California Men’s Colony West in 1966. Photos courtesy of CDCR.')
   }else if(scroll < $('#scroll-img-six').offset().top && scroll >$('#scroll-img-five').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/5.jpg")
+    $('.img-scroller .caption').html('Fire camp inmates learn rescuing techniques. Photo estimated to be taken during the 1960s. Photo courtesy of CDCR.')
   }
 
   if (scroll > $('#scroll-img-seven').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/7.jpg")
+    $('.img-scroller .caption').html('California Men’s Colony kitchen, 1974.')
   }else if(scroll < $('#scroll-img-seven').offset().top && scroll >$('#scroll-img-six').offset().top){
     $(".img-scroller img").attr("src","./assets/archival/6.jpg")
+    $('.img-scroller .caption').html('Inmates process tobacco at a factory at California Men’s Colony West in 1966. Photos courtesy of CDCR.')
   }
 
   // when the top of the #end-scroll div reaches the top of the page
