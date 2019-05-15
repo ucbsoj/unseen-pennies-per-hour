6751998ee7be61a7fa145ddf352710f2e0ea28a8
Lucio Villa <lucio310@gmail.com>
2019-05-15 08:14:46 -0700

    updated image scrolling on desktop and replaced it with a slider on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import $ from "jquery"; // importing jQuery, you can delete if not needed
 require("./lib/social"); // Twitter intent JS
+import Flickity from "flickity";
 import s from 'skrollr';
+
+var flky = new Flickity( '.carousel', {});
 
 $(document).scroll(function() {
 
